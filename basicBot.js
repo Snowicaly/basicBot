f1303a3de15ea2ca05a25366d6bccf6b611e0a1e
Snowicaly <animetears@live.se>
2015-07-31 19:33:35 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -240,7 +240,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Snowicaly/basicBot/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
